d0b8f45209a4309f66dff4025818a51d3e9fa498
mark19242 <mark19242@gmail.com>
2024-04-01 16:32:38 -0700

    model specs updated

--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -5,6 +5,18 @@ RSpec.describe Cat, type: :model do
         cat = Cat.create(age: 2, enjoys:'Walks in the park and drinking milk', image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80')
         expect(cat.errors[:name]).to_not be_empty
       end
+    it "should validate age" do
+        cat = Cat.create(name: 'Whiskers', enjoys:'Walks in the park and drinking milk', image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80', age: nil)
+        expect(cat.errors[:age]).to_not be_empty
+      end
+    it "should validate enjoys" do
+        cat = Cat.create(name: 'Whiskers', age: 2, image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80', enjoys: 'Too short')
+        expect(cat.errors[:enjoys]).to_not be_empty
+      end
+    it "should validate image" do
+        cat = Cat.create(name: 'Whiskers', age: 2, enjoys:'Walks in the park and drinking milk')
+        expect(cat.errors[:image]).to_not be_empty
+      end
       
       it 'is valid if enjoys is at least 10 characters' do
         cat = Cat.create(name: 'mark', age: 4, enjoys:'Chasing mice and killing birds', image: 'gibberish')
